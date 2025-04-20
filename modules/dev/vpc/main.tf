f30657c6871c31675e1903a607b5eb6a22215a42
Thomas <pavan_thomas@yahoo.com>
2025-04-20 16:17:23 -0700

    Create main.tf

---
+++ modules/dev/vpc/main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "instance_subnet" {
+    vpc_id              = aws_vpc.main_vpc.id
+    availability_zone   = var.az
+    cidr_block          = var.subnet_cidr_block
+}
+
+resource "aws_internet_gateway" "instance_igw" {
+    vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "instance_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.instance_igw.id
+  }
+}
+
+resource "aws_route_table_association" "instance" {
+  subnet_id      = aws_subnet.instance_subnet.id
+  route_table_id = aws_route_table.instance_rt.id
+}
